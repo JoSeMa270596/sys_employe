24a17bcc8fe3520037eb8fb05981ce44fec258d9
JoSeMa270596 <112894489+JoSeMa270596@users.noreply.github.com>
2025-06-09 18:02:07 -0400

    feat(frontend): Add menu navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DepartmentComponent } from './features/department/department.component';
+import { EmployeeComponent } from './features/employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'department', component: DepartmentComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: '', redirectTo: '/employee', pathMatch: 'full' },
+  { path: '**', redirectTo: '/employee' }
+];
