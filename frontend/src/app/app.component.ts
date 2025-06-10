24fa9dd21e16e6fe18f41370d2407a704dd2d5cf
JoSeMa270596 <112894489+JoSeMa270596@users.noreply.github.com>
2025-06-09 22:23:43 -0400

    feat(auth): Add login and logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from './features/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,9 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Empleados';
+  #authService = inject(AuthService);
+  title = 'BTSoft';
+  logout() {
+    this.#authService.logout();
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { DepartmentComponent } from './features/department/department.component';
 import { EmployeeComponent } from './features/employee/employee.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { authGuard } from './features/auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'department', component: DepartmentComponent },
-  { path: 'employee', component: EmployeeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'department', component: DepartmentComponent, canActivate: [authGuard] },
+  { path: 'employee', component: EmployeeComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: '**', redirectTo: '/employee' }
 ];
